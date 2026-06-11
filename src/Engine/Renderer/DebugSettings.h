0077c02286a64af32cc72b96ad76ad20aaf12fc3
Adrian <seraphzp@gmail.com>
2026-06-11 12:25:01 +0900

    Debug Input + Debug Settings Cleanup

---
+++ src/Engine/Renderer/DebugSettings.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Engine
+{
+    struct DebugSettings final
+    {
+        bool wireframeEnabled = false;
+        bool grayscaleEnabled = false;
+        bool vignetteEnabled = false;
+    };
+}
